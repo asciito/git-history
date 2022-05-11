e9be7faccf14d57d5f29a1b2addd1a8b1aab56fd
asciito <ayaxdev.mx@gmail.com>
2022-05-11 13:28:39 -0500

    Fetching the data from GitHub, and using Server Side Rendering for the current page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
-export default function Home() {
-  return (
-      <h1>Hello, World!</h1>
-  )
+export default function Index({data}) {
+    return (
+        <h1>Hello, world</h1>
+    )
+}
+
+export async function getServerSideProps() {
+    // Get the commits history
+    const res = await fetch(`${process.env.GITHUB_API}/repos/${process.env.GITHUB_USER}/${process.env.GITHUB_REPO}/commits`);
+    const data = await res.json();
+
+    return {props: {data}};
 }
