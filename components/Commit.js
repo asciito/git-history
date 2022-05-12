d3ffd6b29ada2dd42efa2e2fb2b29fdacea3ee8b
asciito <ayaxdev.mx@gmail.com>
2022-05-11 19:11:41 -0500

    Quick refactoring
    I think it would be a good idea to have access (at least been redirected) to the commit on GitHub, so I only added an external link to GitHub, and of course a quick formatting with PHPStorm.

--- components/Commit.js
+++ components/Commit.js
@@ -14,19 +14,26 @@ export default function Commit({data}) {
     return (
         <li className="shadow p-4 rounded-sm border border-gray-100 relative focus:outline-none focus:ring focus:ring-blue-200"
             tabIndex="0">
-            <span className="focus:ring focus:ring-blue-400">{title}</span>
+            <span className="focus:ring focus:ring-blue-400">
+                {title} {' '}
+                <a
+                    title="Go to the commit page"
+                    className="text-[.8rem] text-gray-400 hover:text-blue-400 active:text-blue-500"
+                    style={{letterSpacing: '1px'}}
+                    target="_blank" href={data.html_url}>({data.sha.substring(0, 6)})</a>
+            </span>
 
             {description.length ?
                 <>
                     <span
                         className="absolute inline-block cursor-pointer right-0 top-0 pt-[3px] mr-4 mt-4 select-none text-xl font-bold text-blue-400"
                         onClick={() => setShowDescription(!showDescription)}>
-                        <IoIosArrowDown className={ 'transition-all ' + (!showDescription ? '-rotate-90' : '') }/>
+                        <IoIosArrowDown className={'transition-all ' + (!showDescription ? '-rotate-90' : '')}/>
                     </span>
 
                     <div
                         className={[!showDescription ? 'hidden' : '', 'shadow-md shadow-inner p-3 mt-5 rounded-sm border border-gray-100'].join(' ')}>
-                        <h3 className="text-lg font-medium mb-2">Description </h3>
+                        <h3 className="text-lg font-medium mb-2">Description</h3>
                         <CommitDescription descriptions={description}/>
                     </div>
                 </>
